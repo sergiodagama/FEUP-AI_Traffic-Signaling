d97409b2e157e7704848f65fa9bc08125a7f34ee
frenato00 <up201908044@up.pt>
2022-04-19 12:04:07 +0100

    added minor documentation a refactoring to Simulation

--- src/main.py
+++ src/main.py
@@ -7,27 +7,29 @@ from GloriousEvolution import GloriousEvolution
 
 def main():
     city_plan_data = parse_input_file("docs/city_plans/city_plan_1.txt")
-    sim = Simulation(city_plan_data,"path", "docs/schedules/schedule1.txt")
-    # sim = Simulation("docs/city_plans/city_plan_1.txt","random")
-
-    sim.run()
-    sim.output_state_file("docs/schedules/result.txt", 'w')
-    print("The Simulation score is: " + str(sim.score))
-
-    new_state = sim.output_state()
-    # new_state[0],new_state[1] = new_state[1],new_state[0]
-    new_state[0].traffic_lights[0].time = 1
-    sim.set_state(new_state)
-    sim.reset()
-
-    sim.run()
-    print("sim2 score: " + str(sim.score))
-    sim.output_state_file("docs/schedules/result2.txt", 'w')
 
 
+    # sim = Simulation(city_plan_data,"path", "docs/schedules/schedule1.txt")
+    # # sim = Simulation(city_plan_data,"random")
 
-    # ev = GloriousEvolution("docs/city_plans/city_plan_1.txt", 100)
-    # ev.run_current()
+    # sim.run()
+    # sim.output_state_file("docs/schedules/result.txt", 'w')
+    # print("The Simulation score is: " + str(sim.score))
+
+    # new_state = sim.output_state()
+    # # new_state[0],new_state[1] = new_state[1],new_state[0]
+    # new_state[0].traffic_lights[0].time = 1
+    # sim.set_state(new_state)
+    # sim.reset()
+
+    # sim.run()
+    # print("sim2 score: " + str(sim.score))
+    # sim.output_state_file("docs/schedules/result2.txt", 'w')
+
+
+
+    ev = GloriousEvolution(city_plan_data)
+    ev.run_current()
 
 
 
