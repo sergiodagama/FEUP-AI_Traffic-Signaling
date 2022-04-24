aa830ada05e092c8a70566970bead5d4b8b87e23
Sérgio da Gama <sergio.gama@smartex.ai>
2022-04-24 12:47:58 +0100

    simulated annealing non monotonic cooling done

--- src/SimulatedAnnealing.py
+++ src/SimulatedAnnealing.py
@@ -18,12 +18,6 @@ class AddMonotonicCooling:
         pass
 
 
-class NonMonotonicAdaptCooling:
-    def get_temperature(self, t0: float, tn: float) -> str:
-        """Override"""
-        pass
-
-
 """ Multiplicative Monotonic Cooling """
 
 
@@ -60,31 +54,44 @@ class QuadAdd(AddMonotonicCooling):
         return tn + (t0 - tn) * (((n - i) / n) ** 2)
 
 
+""" Non-Monotonic Adaptive Cooling """
+
+
+class NonMonotonicAdaptCooling:
+    def get_temperature(self, current_score, best_score, t):
+        return (1 + (current_score - best_score) / current_score) * t
+
+
 """ Simulated Annealing optimization algorithm """
 
 
 class SimulatedAnnealing:
-    def __init__(self, simulation, t_schedule_type):
+    def __init__(self, simulation):
         self.simulation = simulation
-        self.t_schedule_type = t_schedule_type
 
-    def temp_schedule(self, t_schedule_type, t0, i, tn, n):
+    def temp_schedule(self, t_schedule_type, t0, i, tn, n, is_non_mono, current_score, best_score):
         alpha = 0.85
+        t = 0
 
         if t_schedule_type == 1:
-            return ExponentialMult.get_temperature(self, t0, alpha, i)
+            t = ExponentialMult.get_temperature(self, t0, alpha, i)
         elif t_schedule_type == 2:
-            return LogarithmicMult.get_temperature(self, t0, 2, i)
+            t = LogarithmicMult.get_temperature(self, t0, 2, i)
         elif t_schedule_type == 3:
-            return LinearMult.get_temperature(self, t0, alpha, i)
+            t = LinearMult.get_temperature(self, t0, alpha, i)
         elif t_schedule_type == 4:
-            return QuadraticMult.get_temperature(self, t0, alpha, i)
+            t = QuadraticMult.get_temperature(self, t0, alpha, i)
         elif t_schedule_type == 5:
-            return LinearAdd.get_temperature(self, t0, tn, i, n)
+            t = LinearAdd.get_temperature(self, t0, tn, i, n)
         elif t_schedule_type == 6:
-            return QuadAdd.get_temperature(self, t0, tn, i, n)
+            t = QuadAdd.get_temperature(self, t0, tn, i, n)
         else:
             print("ERROR IN TEMP SCHEDULE")
+
+        if is_non_mono:
+            return NonMonotonicAdaptCooling.get_temperature(self, current_score, best_score, t)
+
+        return t
 
     def random_neighbour(self, inter_prob, light_prob):
         for inter in self.simulation.intersections:
@@ -107,7 +114,7 @@ class SimulatedAnnealing:
         self.simulation.run()
         return self.simulation.score
 
-    def run(self, n_iterations, t0, schedule_type, tn):
+    def run(self, n_iterations, t0, schedule_type, tn, is_non_mono):
         best = self.simulation.output_state_copy()
         best_score = self.value(best)
 
@@ -116,14 +123,15 @@ class SimulatedAnnealing:
         s_values = []
 
         for i in range(n_iterations):
-            t = self.temp_schedule(schedule_type, t0, i, tn, n_iterations)
+            current_state = self.simulation.output_state_copy()
+            current_score = self.value(current_state)
+
+            t = self.temp_schedule(schedule_type, t0, i, tn, n_iterations, is_non_mono, current_score, best_score)
 
             t_values.append(t)
             i_values.append(i)
 
-            current_state = self.simulation.output_state_copy()
-
-            s_values.append(self.value(current_state))
+            s_values.append(current_score)
 
             self.random_neighbour(0.1, 0.1)
             next_state = self.simulation.output_state_copy()
@@ -136,7 +144,7 @@ class SimulatedAnnealing:
 
             print("iteration: [" + str(i) + "]" + " best score: " + str(best_score) + " temperature: " + str(t) + " acceptance prob: " + str(self.accept_with_prob(values_diff, t)), end="\n")
             # print("\ncurr state: ", self.simulation.print_state(current_state), end="\n")
-            # print(" next state: ", self.simulation.print_state(next_state), end="\n")
+            # print("\nnext state: ", self.simulation.print_state(next_state), end="\n")
 
             if values_diff >= 0:
                 current_state = next_state
@@ -146,7 +154,8 @@ class SimulatedAnnealing:
 
             self.simulation.set_state(current_state)
 
-        plt.plot(i_values, s_values)
+        plt.plot(i_values, s_values, color='green', linewidth=2,
+                     marker='o', markerfacecolor='blue', markersize=4)
         plt.ylabel('Score')
         plt.xlabel('Iteration')
         plt.show()
